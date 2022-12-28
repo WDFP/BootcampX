739581014e64ecfe3992524704203abd2100cf89
Victor Ha <victorha88@gmail.com>
2022-12-28 12:44:54 -0500

    BootcampX Queries #4

---
+++ 4_queries/2_total_student_assistance_request.sql
@@ -0,0 +1,6 @@
+SELECT count(assistance_requests.*) as total_assistances, students.name
+FROM students
+JOIN assistance_requests
+ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
